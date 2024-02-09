18af75ac2879188f9a9fba72d71da1ed87c9cd98
Vasily Bobnev <ardxel@MacBook-Pro-Vasily.local>
2024-02-09 10:32:22 +0300

    fix("bad code") yc controller

--- api/src/v1/modules/yc/yc.controller.ts
+++ api/src/v1/modules/yc/yc.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { HttpStatusCode } from 'axios';
 import { TranslateDTO } from './dto';
-import { YCResponseListLanguages, YCResponseTranslate } from './interfaces';
+import { YCResponseSupportedLanguageList, YCResponseTranslate } from './interfaces';
 import { YcService } from './yc.service';
 import { ResponseSupportedLanguageList, ResponseTranslate } from 'common/swagger/v1/types';
 
@@ -30,7 +30,7 @@ export class YcController {
         type: ResponseSupportedLanguageList,
         description: 'gets a list of supported languages',
     })
-    async supportedLanguages(): Promise<YCResponseListLanguages> {
-        return await this.supportedLanguages();
+    async supportedLanguages(): Promise<YCResponseSupportedLanguageList> {
+        return await this.ycService.getSupportedLanguageList();
     }
 }
---
+++ api/test/curl/yc/languages/languages.sh
@@ -0,0 +1,2 @@
+curl --request POST \
+	"http://localhost:3111/api/v1/yc/languages"
